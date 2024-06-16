fc4d3d2a190eb897bb3b7e615d33eb0259058435
pqdat21 <pqdat21022k3@gmail.com>
2024-06-16 12:28:37 +0700

    Add Player Movement and Physics

--- Assets/Scrips/Player Controller.cs
+++ Assets/Scrips/Player Controller.cs
@@ -11,10 +11,15 @@ public class PlayerController : MonoBehaviour
     private Vector2 movement;
     private Rigidbody2D rb;
 
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRenderer;
+
     private void Awake()
     {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();   
+        myAnimator = GetComponent<Animator>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnEnable()
@@ -29,14 +34,33 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        AdjustPlayerFacingDirection();
         Move();
     }
+
 
     private void PlayerInput()
     {
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
+
+        myAnimator.SetFloat("moveX", movement.x);
+        myAnimator.SetFloat("moveY", movement.y);
     }
 
+    private void AdjustPlayerFacingDirection()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        if(mousePos.x < playerScreenPoint.x)
+        {
+            mySpriteRenderer.flipX = true;
+        } 
+        else
+        {
+            mySpriteRenderer.flipX = false;
+        }
+    }
     private void Move()
     {
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
